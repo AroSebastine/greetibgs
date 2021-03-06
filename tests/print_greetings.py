100d3bdb863e83ae6fd69f46464acd0487fae17b
Aro Hendric <arosebastinenishanth@gmail.com>
2021-03-07 08:00:24 +1030

    Add translator to Portuguese

--- tests/print_greetings.py
+++ tests/print_greetings.py
@@ -1,8 +1,11 @@
 from page_functions import greetings
+from translate import Translator
+
+translator = Translator(to_lang='pt')
 
 
 print(greetings.aro())
 
 
 for greeting in greetings.Testing().greetings_list():
-    print(greeting.title() + "!")
+    print(translator.translate(greeting).title())
